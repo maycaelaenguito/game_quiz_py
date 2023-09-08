ae16abf64c2b1fffc2f05c7c3f59ec89625302c0
Maycaela Enguito <maycaelaenguito@gmail.com>
2023-09-08 06:23:57 -0400

    quiz test

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # game_quiz_py
 Game Quiz with Python
+
+#### Game Quiz with Python
+
+This is a Python-based quiz game that allows you to create and play a computer-related quiz. You can customize the questions and answers to create your own quiz.
+
+## Getting Started
+# Clone or download this repository to your local machine.
+git clone https://github.com/your-username/game-quiz-python.git
+
+# Open your terminal or command prompt and navigate to the project directory.
+cd game-quiz-python
+
+# Run the Python script to start the quiz game.
+python quiz_game.py
+
+
+### How to Play
+
+# When you start the game, you will be prompted with a welcome message and asked if you want to play.
+
+# If you enter "yes," the quiz will begin. If you enter anything other than "yes," the game will exit.
+
+# You will be presented with a series of multiple-choice questions related to computer programming and technology.
+
+# Enter your answer by typing the corresponding letter (e.g., "a" for option "a"). The game will inform you whether your answer is correct or not.
+
+# At the end of the quiz, your final score will be displayed.
+
+# You can customize the quiz questions and answers by modifying the quiz_game.py script to create your own quiz.
--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,173 @@
 print("Welcome to my computer quiz!")
 
-playing = input("Do you want to play?")
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+score = 0
+
+# Question 1
+print("\nQuestion 1: What does HTML stand for in web development?")
+print("a) Hyper Text Markup Language")
+print("b) High-Level Text Management Language")
+print("c) Hyper Transfer Markup Language")
+print("d) High-Level Text Markup Language")
+
+answer1 = input("Your answer (a/b/c/d): ")
+if answer1.lower() == "a":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'a) Hyper Text Markup Language'.")
+
+
+# Question 2
+print("\nQuestion 2: Which programming language is known for its use in machine learning and data analysis?")
+print("a) Java")
+print("b) Python")
+print("c) C++")
+print("d) Ruby")
+
+answer2 = input("Your answer (a/b/c/d): ")
+if answer2.lower() == "b":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'b) Python'.")
+
+
+# Question 3
+print("\nIn JavaScript, which keyword is used to declare a variable with block ")
+print("a) var")
+print("b) let")
+print("c) const")
+print("d) function")
+
+answer3 = input("Your answer (a/b/c/d): ")
+if answer3.lower() == "b":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'b) let'.")
+
+
+# Question 4
+print("\nWhat does SQL stand for in database management? ")
+print("a) Structured Query Language")
+print("b) Sequential Query Language")
+print("c) System Query Language")
+print("d) Server Query Language")
+
+answer4 = input("Your answer (a/b/c/d): ")
+if answer4.lower() == "a":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'a) Structured Query Language'.")
+
+
+# Question 5
+print("\nWhich data structure follows the Last-In-First-Out (LIFO) principle? ")
+print("a) Queue")
+print("b) Stack")
+print("c) Linked List")
+print("d) Array")
+
+answer5 = input("Your answer (a/b/c/d): ")
+if answer5.lower() == "b":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'b) Stack'.")
+
+
+# Question 6
+print("\nIn object-oriented programming, what is the process of creating a new instance of a class called? ")
+print("a) Inheritance")
+print("b) Abstraction")
+print("c) Instantiation")
+print("d) Polymorphism")
+
+answer6 = input("Your answer (a/b/c/d): ")
+if answer6.lower() == "c":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'c) Instantiation'.")
+
+
+# Question 7
+print("\nWhich programming paradigm focuses on describing what a program should do, rather than how to do it? ")
+print("a) Functional programming")
+print("b) Object-oriented programming")
+print("c) Procedural programming")
+print("d) Imperative programming")
+
+answer7 = input("Your answer (a/b/c/d): ")
+if answer7.lower() == "a":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'a) Functional programming'.")
+
+
+# Question 8
+print("\nWhich sorting algorithm has an average and worst-case time complexity of O(n^2)? ")
+print("a) Quick Sort")
+print("b) Merge Sort")
+print("c) Insertion Sort")
+print("d) Bubble Sort")
+
+answer8 = input("Your answer (a/b/c/d): ")
+if answer8.lower() == "d":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'd) Bubble Sort'.")
+
+
+# Question 9
+print("\nWhat is the primary purpose of version control systems like Git? ")
+print("a) To write and execute code")
+print("b) To document code comments")
+print("c) To track changes in code and collaborate with others")
+print("d) To optimize code for performance")
+
+answer9 = input("Your answer (a/b/c/d): ")
+if answer9.lower() == "c":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'c) To track changes in code and collaborate with others'.")
+
+
+# Question 10
+print("\nWhich HTTP status code indicates a successful request in web development? ")
+print("a) 200 OK")
+print("b) 404 Not Found")
+print("c) 500 Internal Server Error")
+print("d) 302 Found")
+
+answer10 = input("Your answer (a/b/c/d): ")
+if answer10.lower() == "a":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is 'a) 200 OK' ")
+
+# Display the final score
+print("\nYour final score is:", score)
+print("\nCorrect Answers:")
+print("1. a) Hyper Text Markup Language")
+print("2. b) Python")
+print("3. b) let")
+print("4. a) Structured Query Language")
+print("5. b) Stack")
+print("6. c) Instantiation")
+print("7. a) Functional programming")
+print("8. d) Bubble Sort")
+print("9. c) To track changes in code and collaborate with others")
+print("10. a) 200 OK")
